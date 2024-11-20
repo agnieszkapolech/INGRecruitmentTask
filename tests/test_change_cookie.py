dd0c35e0826c30b037ca1554f8f31bb691561ef9
Agnieszka Polech <agnieszka.polech@orange.com>
2024-11-20 20:39:12 +0100

    cookie changed

---
+++ tests/test_change_cookie.py
@@ -0,0 +1,27 @@
+def test_change_cookie(page):
+
+    page.goto("https://www.ing.pl/")
+    page.get_by_role("button", name="Dostosuj").click()
+    page.locator("label.cookie-policy-switch-label").nth(2).click()
+
+    element = page.locator("label.cookie-policy-switch-label input#CpmAnalyticalOption")
+    name_value = element.get_attribute("name")
+    expected_name = "CpmAnalyticalOption"  # Wartość, którą chcesz sprawdzić
+
+    assert name_value == expected_name, f"Błąd! Oczekiwana wartość: {expected_name}, a znaleziono: {name_value}"
+    print("Poprawnie wybrałeś Cookies analityczne!")
+
+    page.get_by_role("button", name="Zaakceptuj wybrane").click()
+
+    assert page.inner_text(
+        "text=Twoje ustawienia zostały zapisane") == "Twoje ustawienia zostały zapisane", f"Actuall value: {page.inner_text('text = Twoje ustawienia zostały zapisane')}"
+
+    banner_selector = "span"
+    banner = page.locator(banner_selector).filter(has_text="Twoje ustawienia zostały zapisane")
+
+    assert banner.is_visible(), "Baner z komunikatem 'Twoje ustawienia zostały zapisane' nie jest widoczny!"
+    print("Baner 'Twoje ustawienia zostały zapisane' jest widoczny.")
+
+    banner.wait_for(state="hidden", timeout=6000)       #czas ustawiony tak, aby test działał dla trzech przegądarek
+    assert banner.is_hidden(), "Baner nie zniknął po czasie oczekiwania!"
+    print("Baner zniknął poprawnie po kilku sekundach.")
